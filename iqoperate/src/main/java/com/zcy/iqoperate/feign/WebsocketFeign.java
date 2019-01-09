fcefb7947ad7b6e637d9a64b129dc931c54a0cc2
zcy <xiadao12@yeah.net>
2019-01-09 19:36:00 +0800

    修改

--- iqoperate/src/main/java/com/zcy/iqoperate/feign/WebsocketFeign.java
+++ iqoperate/src/main/java/com/zcy/iqoperate/feign/WebsocketFeign.java
@@ -5,12 +5,9 @@ import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
- * @author 苏州中科蓝迪公司所有(c)2016-2021
- * @version 1.0.0
- * @brief ecs insight
- * @note 修订历史： 1、yangzhouchuan于2019/1/9设计并构建初始版本v1.0.0
+ * 2019/1/9
  */
-@FeignClient("iqconnect")
+@FeignClient(value="iqconnect")
 public interface WebsocketFeign {
     /**
      * 向websocket发送请求消息
--- iqoperate/src/main/java/com/zcy/iqoperate/service/WebsocketService.java
+++ iqoperate/src/main/java/com/zcy/iqoperate/service/WebsocketService.java
@@ -1,10 +1,7 @@
 package com.zcy.iqoperate.service;
 
 /**
- * @author 苏州中科蓝迪公司所有(c)2016-2021
- * @version 1.0.0
- * @brief ecs insight
- * @note 修订历史： 1、yangzhouchuan于2019/1/9设计并构建初始版本v1.0.0
+ * 2019/1/9
  */
 public interface WebsocketService {
 }
