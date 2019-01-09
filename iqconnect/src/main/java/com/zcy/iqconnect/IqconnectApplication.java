2e4cd0c10cfabe7246386e96a1be9fee5d4e044d
xiadao12 <xiadao12@yeah.net>
2019-01-09 18:36:23 +0800

    修改

--- iqconnect/src/main/java/com/zcy/iqconnect/IqconnectApplication.java
+++ iqconnect/src/main/java/com/zcy/iqconnect/IqconnectApplication.java
@@ -8,9 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
 public class IqconnectApplication {
 
     private static Logger logger = LoggerFactory.getLogger(IqconnectApplication.class);
