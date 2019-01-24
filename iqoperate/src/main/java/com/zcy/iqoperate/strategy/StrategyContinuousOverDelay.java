df0ae49a21cc1972376f1ab7cbd291d2029dbd4e
zcy <xiadao12@yeah.net>
2019-01-24 20:58:18 +0800

    修改

--- iqoperate/src/main/java/com/zcy/iqoperate/strategy/StrategyContinuousOverDelay.java
+++ iqoperate/src/main/java/com/zcy/iqoperate/strategy/StrategyContinuousOverDelay.java
@@ -32,21 +32,12 @@ public class StrategyContinuousOverDelay {
         //连续蜡烛的涨跌方向
         Integer continuousTrend = null;
 
-        //记录输赢的次数
-        Integer winNum = 0;
-        Integer lostNum = 0;
-
-        //记录赢输的时间点
-        List<String> winTimeList = new ArrayList<>();
-        List<String> lostTimeList = new ArrayList<>();
-
-
-
         //延迟几分钟购买
-        for (int delayMinutes = 0; delayMinutes <= 5; delayMinutes++) {
+        for (int delayMinutes = 2; delayMinutes <= 5; delayMinutes++) {
             String content = "";
+
             //连续蜡烛图个数以上（包含）
-            for (int minCandlenum = 4; minCandlenum <= 8; minCandlenum++) {
+            for (int minCandlenum = 8; minCandlenum <= 8; minCandlenum++) {
 
                 //反向的蜡烛图长度因子
                 for(BigDecimal factor = new BigDecimal(0.00001); factor.compareTo(new BigDecimal(0.0002)) <= 0; factor = factor.add(new BigDecimal(0.00001))){
@@ -60,6 +51,14 @@ public class StrategyContinuousOverDelay {
 
                     System.out.println("反向的蜡烛图长度因子 = " + factor);
                     content = content + "反向的蜡烛图长度因子  = " + factor;
+
+                    //记录输赢的次数
+                    Integer winNum = 0;
+                    Integer lostNum = 0;
+
+                    //记录赢输的时间点
+                    List<String> winTimeList = new ArrayList<>();
+                    List<String> lostTimeList = new ArrayList<>();
 
                     //遍历蜡烛
                     for (int k=0;k<candles.size() - delayMinutes;k++) {
@@ -128,20 +127,20 @@ public class StrategyContinuousOverDelay {
                     }
 
                     System.out.println("winNum = " + winNum);
-                    System.out.println("winTimeList = " + winTimeList);
                     System.out.println("lostNum = " + lostNum);
+                    System.out.println("winTimeList = " + winTimeList);
                     System.out.println("lostTimeList = " + lostTimeList);
 
-                    content = content + "winNum = " + lostNum;
-                    content = content + "winTimeList = " + lostTimeList;
-                    content = content + "lostNum = " + lostNum;
-                    content = content + "lostTimeList = " + lostTimeList;
+                    content = content + "\nwinNum = " + winNum + "\n";
+                    content = content + "lostNum = " + lostNum + "\n";
+                    content = content + "winTimeList = " + lostTimeList + "\n";
+                    content = content + "lostTimeList = " + lostTimeList + "\n";
                 }
             }
 
             //创建策略结果文件
             if (strategyContinuousDoubleFilter.getCreateResultFile()) {
-                FileUtil.createJsonFile(content, "D:/iq", "StrategyContinuousOverDelay_"+ delayMinutes + strategyContinuousDoubleFilter.getActiveId() + ".json");
+                FileUtil.createFile(content, "D:/iq", "StrategyContinuousOverDelay_"+ delayMinutes + strategyContinuousDoubleFilter.getActiveId() + ".json");
             }
         }
 
